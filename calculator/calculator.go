cab64798f16f7ef7a559c3c4b9ff9d18fb3256df
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-04-25 10:52:44 -0600

    implements divide and sqrt

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,6 +1,11 @@
 // Package calculator does simple calculations
 package calculator
 
+import (
+	"errors"
+	"math"
+)
+
 // Add takes two integers and returns the sum of them
 func Add(a, b float64) float64 {
 	return a + b
@@ -12,4 +17,18 @@ func Subtract(a, b float64) float64 {
 
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0.0, errors.New("cannot divide by zero")
+	}
+	return a / b, nil
+}
+
+func Sqrt(a float64) (float64, error) {
+	if a < 0 {
+		return 0, errors.New("cannot take sqrt of a negative number")
+	}
+	return math.Sqrt(a), nil
 }
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -2,6 +2,7 @@ package calculator_test
 
 import (
 	"calculator"
+	"math"
 	"testing"
 )
 
@@ -41,7 +42,6 @@ func TestSubtract(t *testing.T) {
 		got := calculator.Subtract(tc.a, tc.b)
 		if tc.want != got {
 			t.Errorf("wanted: %f, got: %f", tc.want, got)
-
 		}
 	}
 }
@@ -61,7 +61,73 @@ func TestMultiply(t *testing.T) {
 		got := calculator.Multiply(tc.a, tc.b)
 		if tc.want != got {
 			t.Errorf("wanted: %f, got: %f", tc.want, got)
-
 		}
 	}
+}
+
+func closeEnough(a, b, tolerance float64) bool {
+	return math.Abs(a-b) <= tolerance
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 5, b: 2, want: 2.5},
+		{a: 1, b: -1, want: -1},
+		{a: -1, b: -1, want: 1},
+		{a: 1, b: 3, want: 0.333333},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+		if err != nil {
+			t.Fatalf("want no error for valid input, got %v", err)
+		}
+		if !closeEnough(tc.want, got, 0.001) {
+			t.Errorf("Divide(%f, %f): want %f, got: %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestDivideInvalid(t *testing.T) {
+	t.Parallel()
+	_, err := calculator.Divide(5, 0)
+	if err == nil {
+		t.Error("wanted err but got none")
+	}
+
+}
+
+func TestSqrt(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a    float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 25, want: 5},
+		{a: 0, want: 0},
+		{a: 5, want: 2.230607},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Sqrt(tc.a)
+		if err != nil {
+			t.Fatalf("want no error for valid input, got %v", err)
+		}
+		if !closeEnough(tc.want, got, 0.01) {
+			t.Errorf("Sqrt(%f): want %f, got: %f", tc.a, tc.want, got)
+		}
+	}
+}
+
+func TestSqrtInvalid(t *testing.T) {
+	t.Parallel()
+	_, err := calculator.Sqrt(-5)
+	if err == nil {
+		t.Error("wanted err but got none")
+	}
+
 }
