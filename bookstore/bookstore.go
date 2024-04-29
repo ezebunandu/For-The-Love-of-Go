fba4b8d61334d2cff7197afcd0c0c7b3478d1003
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-04-28 23:19:33 -0600

    user can search for a book in the catalog and get a list of all books

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -1,6 +1,9 @@
 package bookstore
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type Book struct {
 	Title  string
@@ -17,15 +20,18 @@ func Buy(b Book) (Book, error) {
 	return b, nil
 }
 
-func GetAllBooks(c []Book) []Book {
-	return c
+func GetAllBooks(catalog map[int]Book) []Book {
+	result := []Book{}
+	for _, b := range catalog {
+		result = append(result, b)
+	}
+	return result
 }
 
-func GetBook(catalog []Book, i int) Book {
-	for _, b := range catalog {
-		if b.ID == i {
-			return b
-		}
+func GetBook(catalog map[int]Book, ID int) (Book, error) {
+	book, ok := catalog[ID]
+	if !ok {
+		return Book{}, fmt.Errorf("ID %d doesn't exist", ID)
 	}
-	return Book{}
+	return book, nil
 }
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -2,6 +2,7 @@ package bookstore_test
 
 import (
 	"bookstore"
+	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -49,15 +50,18 @@ func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
 
 func TestGetAllBooks(t *testing.T) {
 	t.Parallel()
-	catalog := []bookstore.Book{
-		{Title: "For the Love of Go"},
-		{Title: "The Power of Go: Tools"},
+	catalog := map[int]bookstore.Book{
+		1: {Title: "For the Love of Go"},
+		2: {Title: "The Power of Go: Tools"},
 	}
 	want := []bookstore.Book{
 		{Title: "For the Love of Go"},
 		{Title: "The Power of Go: Tools"},
 	}
 	got := bookstore.GetAllBooks(catalog)
+	sort.Slice(got, func(i, j int) bool {
+		return got[i].ID < got[j].ID
+	})
 	if !cmp.Equal(want, got) {
 		t.Error(cmp.Diff(want, got))
 	}
@@ -65,13 +69,25 @@ func TestGetAllBooks(t *testing.T) {
 
 func TestGetBook(t *testing.T) {
 	t.Parallel()
-	catalog := []bookstore.Book{
-		{Title: "For the Love of Go", ID: 1},
-		{Title: "The Power of Go: Tools", ID: 2},
+	catalog := map[int]bookstore.Book{
+		1: {Title: "For the Love of Go", ID: 1},
+		2: {Title: "The Power of Go: Tools", ID: 2},
 	}
 	want := bookstore.Book{Title: "For the Love of Go", ID: 1}
-	got := bookstore.GetBook(catalog, 1)
+	got, _ := bookstore.GetBook(catalog, 1)
 	if !cmp.Equal(want, got) {
 		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestGetBookInvalidID(t *testing.T) {
+	t.Parallel()
+	catalog := map[int]bookstore.Book{
+		1: {Title: "For the Love of Go", ID: 1},
+		2: {Title: "The Power of Go: Tools", ID: 2},
+	}
+	_, err := bookstore.GetBook(catalog, 3)
+	if err == nil {
+		t.Fatal("want error for non-existent ID, got nil")
 	}
 }
